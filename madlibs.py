a9c2fea2925dc78b852af85227492ff227055d6e
Linus Trahair <linus.trahair1@marist.edu>
2018-09-06 14:41:35 -0400

    Solution to MadLibs exercise

---
+++ madlibs.py
@@ -0,0 +1,11 @@
+#CMPT 120L 113
+#MadLibs
+#Author: Linus Trahair
+#Date: 6th Sep 2018
+
+n=input('Enter a noun: ')
+v=input('Enter a verb: ')
+a=input('Enter an adjective: ')
+p=input('Enter a place: ')
+
+print("The "+a+" "+n+" went "+v+" at the "+p)
