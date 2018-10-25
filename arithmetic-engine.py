322e8cfb40ea8a3e4f69378ba4ca2451d037c576
Linus Trahair <linus.trahair1@marist.edu>
2018-10-25 14:36:04 -0400

    added in error handling and general features for the arithemetic engine.

---
+++ arithmetic-engine.py
@@ -0,0 +1,48 @@
+# CMPT 120 - Lab #6
+# YOUR NAME
+# DD-MMM-YYYY
+###
+def showIntro():
+    print("Welcome to the Arithmetic Engine!")
+    print("=================================\n")
+    print("Valid commands are 'add', 'mult', 'sub', 'div', and 'quit'.\n")
+def showOutro():
+    print("\nThank you for using the Arithmetic Engine…")
+    print("\nPlease come back again soon!")
+def doLoop():
+    while True:
+        cmd = input("What computation do you want to perform? ").lower()
+        if cmd =="quit":
+            break
+        elif not cmd=="add" and not cmd=="sub" and not cmd=="mult" and not cmd=="div":
+            print("invalid command, use add, sub, mult, div, or quit.")
+            continue
+        else:
+            
+            try:
+                num1 = int(input("Enter the first number: "))
+                num2 = int(input("Enter the second number: "))
+            except:
+                print("the command you enterend was not valid try again\n")
+                continue
+            if cmd == "add":
+                result = num1 + num2
+            elif cmd == "sub":
+                result = num1 - num2
+            elif cmd == "mult":
+                result = num1 * num2
+            elif cmd == "div":
+                try:
+                    result = num1 / num2
+                except:
+                    print("can't divide by zero\n")
+                    continue
+          
+               
+            
+        print("The result is " + str(result) + ".\n")
+def main():
+    showIntro()
+    doLoop()
+    showOutro()
+main()
